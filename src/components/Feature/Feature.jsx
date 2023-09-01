c855f718ac9cba4e339a71eec30c941cca369a2b
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-02 02:37:21 +0600

    features section

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import './Feature.css'
-const Feature = () => {
+import './Feature.css';
+
+const Feature = ({ title, text }) => {
     return (
-        <div>
-            Feature
+        <div className='gpt3__features-container__feature'>
+            <div className="gpt3__features-container__feature-title">
+                <div></div>
+                <h1>{title}</h1>
+            </div>
+            <div className="gpt3__features-container__feature-text">
+                <p>{text}</p>
+            </div>
         </div>
     );
 };
--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './WhatGPT3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            WhatGPT3
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+            <div className="gpt3__whatgpt3-feature">
+                <Feature title={'What is GPT-3?'} text={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor libero unde qui incidunt sequi amet possimus quas exercitationem necessitatibus provident asperiores nobis totam laborum dolorum error, commodi maxime id eligendi?'} ></Feature>
+            </div>
+            <div className='gpt3__whatgpt3-heading'>
+                <h1 className='gradient__text'>Possibilities Are Beyond Your Imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature title={'Chatbots'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut et voluptatibus saepe qui minima perferendis dignissimos quisquam doloribus totam sed?'}></Feature>
+                <Feature title={'Knowledge Base'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut et voluptatibus saepe qui minima perferendis dignissimos quisquam doloribus totam sed?'} ></Feature>
+                <Feature title={'Education'} text={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut et voluptatibus saepe qui minima perferendis dignissimos quisquam doloribus totam sed?'} ></Feature>
+            </div>
         </div>
     );
 };
